271521f20bc0cf4ba30486f767cb47fa6041ec4d
Ladd J. Hebert <ladd.hebert@gmail.com>
2013-07-10 17:31:42 +0000

    added support to read in index.html to buffer

--- web.js
+++ web.js
@@ -2,8 +2,16 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var index_html = fs.readFileSync('./index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+})
+
+var index_buf = new Buffer(index_html);
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(index_buf.toString());
 });
 
 var port = process.env.PORT || 5000;
